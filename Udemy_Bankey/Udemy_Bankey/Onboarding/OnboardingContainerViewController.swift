fdbb0aad6d4f6eafa8bf3170d8f71ff19b098cf9
yeojinyun <sail041@naver.com>
2022-08-21 22:18:29 +0900

    bankey

--- Udemy_Bankey/Udemy_Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Udemy_Bankey/Udemy_Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class OnboardingContainerViewController: UIViewController {
+
+    
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
     var currentVC: UIViewController {
@@ -36,6 +38,60 @@ class OnboardingContainerViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        addChild(pageViewController) // 1
+        view.addSubview(pageViewController.view) // 2
+        pageViewController.didMove(toParent: self) // 3
+        
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ])
+        
+        
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false)
+        currentVC = pages.first!
+    }
+    
+}
+
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        return getNextViewController(from: viewController)
+    }
+    
+    private func getPreviousViewController(from viewController: UIViewController) ->  UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+    
+    private func getNextViewController(from viewController: UIViewController) ->  UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < 0 else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
 }
 
 class ViewController1: UIViewController {
