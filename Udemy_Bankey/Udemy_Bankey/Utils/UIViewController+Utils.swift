f55f1ff79c285fb333025c762804ba2c368fcb72
yeojinyun <sail041@naver.com>
2022-08-30 23:30:41 +0900

    Udemy

---
+++ Udemy_Bankey/Udemy_Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Utils.swift
+//  Udemy_Bankey
+//
+//  Created by 순진이 on 2022/08/30.
+//
+
+import UIKit
+
+extension UIViewController {
+    // New
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
