2b5bbeb48d778934c5c7500bd084192ddbd0e966
yeojinyun <sail041@naver.com>
2022-09-18 22:22:01 +0900

    udemy

--- Udemy_Bankey/Udemy_Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ Udemy_Bankey/Udemy_Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -15,6 +15,8 @@ class AccountSummaryCell: UITableViewCell {
         case Investment
     }
     
+    
+    
     struct ViewModel {
         let accountType: AccountType
         let accountName: String
