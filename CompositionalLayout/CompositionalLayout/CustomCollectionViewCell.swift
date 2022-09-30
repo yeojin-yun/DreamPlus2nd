757f3c24b75570c8fa2f696308d75bf64d7340d7
yeojinyun <sail041@naver.com>
2022-09-30 18:56:37 +0900

    add customCell

---
+++ CompositionalLayout/CompositionalLayout/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  CompositionalLayout
+//
+//  Created by 순진이 on 2022/09/30.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
