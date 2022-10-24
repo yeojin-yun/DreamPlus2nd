9389731f911471c413916d19037a1fdd3db34e15
yeojinyun <sail041@naver.com>
2022-10-24 22:48:58 +0900

    전처리

---
+++ Preprocessor-Example/Preprocessor-Example/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Preprocessor-Example
+//
+//  Created by 순진이 on 2022/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var server = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 전처리 (preprocessor)
+        // 컴파일이 안되도록 막을 수 있음
+//        #if DEBUG
+//        print("Debug: Debug 모드에서만 보임")
+//        #else
+//        print("")
+//        #endif
+        
+    // 모드마다 변수를 설정할 수도 있음
+    #if TEST
+        server = "Server Test"
+    #elseif DEBUG
+        server = "Debug Server"
+    #elseif QA
+        server = "QA Server"
+    #else
+        server = "Relese Server"
+    #endif
+        
+
+        
+        
+        print(server)
+        
+    }
+    
+    
+}
+
