6a46d9a033a81248ae35a53a89446a3f8a1cea80
yeojinyun <sail041@naver.com>
2022-10-28 22:40:25 +0900

    visualFormatBasic

--- VisualFormatBasic/VisualFormatBasic/ViewController.swift
+++ VisualFormatBasic/VisualFormatBasic/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let redView = UIView()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
     }
-
+    
+    func setUI() {
+        redView.backgroundColor = .red
+        view.addSubview(redView)
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let views = ["R": redView]
+        
+        var constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[R(30)]-|",
+                                                         metrics: nil,
+                                                         views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[R]-30-|",
+                                                      metrics: nil,
+                                                      views: views)
+        
+        view.addConstraints(constraints)
+    }
 
 }
 
