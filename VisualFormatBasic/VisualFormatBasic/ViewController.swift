0603e62761b393a34369726762f9eff517ed6380
yeojinyun <sail041@naver.com>
2022-11-03 23:21:23 +0900

    visual format add

--- VisualFormatBasic/VisualFormatBasic/ViewController.swift
+++ VisualFormatBasic/VisualFormatBasic/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     
     let redView = UIView()
     
-    
+    let greenView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +20,31 @@ class ViewController: UIViewController {
     
     func setUI() {
         redView.backgroundColor = .red
+        greenView.backgroundColor = .green
         view.addSubview(redView)
+        view.addSubview(greenView)
         redView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.translatesAutoresizingMaskIntoConstraints = false
         
-        let views = ["R": redView]
+        let views = ["R": redView, "G": greenView]
         
-        var constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[R(30)]-|",
+        
+        var constraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[R(130)]-|",
                                                          metrics: nil,
                                                          views: views)
         constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[R]-30-|",
                                                       metrics: nil,
                                                       views: views)
         
+        var greenConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[G(30)]-|",
+                                                              metrics: nil,
+                                                              views: views)
+        greenConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[G]-30-|",
+                                                           metrics: nil,
+                                                           views: views)
+        
         view.addConstraints(constraints)
+        view.addConstraints(greenConstraints)
     }
 
 }
