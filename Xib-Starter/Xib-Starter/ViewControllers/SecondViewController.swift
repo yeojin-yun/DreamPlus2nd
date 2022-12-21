8cf2329f1b2f70d07d02668a8769a2efdd74f1e6
yeojinyun <sail041@naver.com>
2022-12-21 23:27:32 +0900

    add second VC

---
+++ Xib-Starter/Xib-Starter/ViewControllers/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Xib-Starter
+//
+//  Created by 순진이 on 2022/12/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
